20947f01437f2344c39dcd56cd77fd583c676df6
zhujiang <ijensenzhu@gmail.com>
2019-01-19 18:21:09 +1300

    finish twitter service

--- scripts/start.js
+++ scripts/start.js
@@ -1,5 +1,77 @@
 'use strict';
 
+const Koa = require('koa');
+
+const router = require('koa-router')();
+
+var cors = require('koa2-cors');
+
+
+const app = new Koa();
+
+var Twit = require('twit')
+
+var T = new Twit({
+  consumer_key:         '2h8wSXBRYHnwXSEwFa7XTrXx5',
+  consumer_secret:      'uUihOfCp45NqVZpojHG2YzfgJ8SKdx2qfIamXUkIdupmyZvORJ',
+  access_token:         '931065980-J8ZTBW5cVTeDYJM7k3dDIeZ8GfmEOWj8Va9dmA0c',
+  access_token_secret:  'KxXwDIg3rFBSBYsjQuGlFFT1dPr10osiWv8I6G862YHaV',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+})
+
+app.use(cors({
+    origin: function (ctx) {
+        return "*"
+    },
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+    maxAge: 5,
+    credentials: true,
+    allowMethods: ['GET', 'POST', 'DELETE'],
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept','X-Requested-With','Origin'],
+}))
+
+// log request URL:
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    await next();
+});
+//?max_id=1086386543386902529&q=Image&count=10&include_entities=1
+// add url-route:
+router.get('/search/tweets/:name/:count/:max_id', async (ctx, next) => {
+    var name = ctx.params.name;
+    var total = ctx.params.count;
+    var maxId = ctx.params.max_id;
+    console.log(name);
+    var promise =T.get('search/tweets', { q: name, count: total, max_id: maxId, include_entities:1});
+    let body = await promise;
+    //ctx.response.body = body.data;
+    
+    var new_search_metadata = body.data.search_metadata;
+
+    var status = body.data.statuses.map((status) => {
+        var newStatus = {
+            id: status.id,
+            created_at: status.created_at,
+            text: status.text,
+            name: status.user.name,
+            profile_image_url: status.user.profile_image_url,
+            media: status.entities.media?status.entities.media:null};
+        return newStatus;
+    });
+    ctx.response.body = {search_metadata:new_search_metadata,statuses:status}
+});
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = '<h1>Index</h1>';
+});
+
+// add router middleware:
+app.use(router.routes());
+
+app.listen(8888);
+console.log('twitterService started at port 8888...');
+
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
